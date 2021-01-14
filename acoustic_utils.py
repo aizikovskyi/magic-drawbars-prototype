b2e80015d428e78da2311aca9acab8dd6b59b2fb
Nick Aizikovskyi <aizikovskyi@users.noreply.github.com>
2021-01-13 16:20:02 -0800

    Fixing conversion error

--- acoustic_utils.py
+++ acoustic_utils.py
@@ -6,4 +6,4 @@ def amplitudeToVolume(amp):
     """Converts amplitude (0-1) to volume (-inf-0)dB"""
     if amp == 0:
         return float('-inf')
-    return 20 * math.log(amp)
+    return 20 * math.log10(amp)
